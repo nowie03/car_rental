f6ef7f5573f4bd06c0027840cd78e07b8b948399
Immanuel <nowpertimmanuel@gmail.com>
2023-05-01 01:03:09 +0530

    front end design done

---
+++ src/Navbar/index.js
@@ -0,0 +1,140 @@
+import { Navbar, Link, Text, Avatar, Dropdown } from "@nextui-org/react";
+import { Layout } from "./Layout";
+import React,{useState} from "react";
+
+
+
+
+
+export default function Nav({isMyListingsSelected,isMyBookingsSelected,isExploreSelected,setIsExploreSelected,setIsMyBookingsSelected,setIsMyListingsSelected}) {
+    //state variables
+  const collapseItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
+  ];
+
+  const handleNavLinkChange=(isSelected)=>{
+    console.log(isSelected);
+  }
+
+  return (
+    <Layout>
+      <Navbar isBordered variant="sticky">
+        <Navbar.Toggle showIn="xs" />
+        <Navbar.Brand
+          css={{
+            "@xs": {
+              w: "12%",
+            },
+          }}
+        >
+          <Text b color="inherit" hideIn="xs">
+            CAR RENTAL
+          </Text>
+        </Navbar.Brand>
+        <Navbar.Content
+          enableCursorHighlight
+          activeColor="secondary"
+          hideIn="xs"
+          variant="highlight-rounded"
+        >
+          <Navbar.Link isActive={isExploreSelected} onClick={()=>{
+            setIsExploreSelected(true)
+            setIsMyBookingsSelected(false)
+            setIsMyListingsSelected(false)
+            }} href="#">Explore</Navbar.Link>
+          <Navbar.Link isActive={isMyBookingsSelected} onClick={()=>{ 
+            setIsMyBookingsSelected(true)
+            setIsExploreSelected(false)
+            setIsMyListingsSelected(false)}}  href="#">My Bookings</Navbar.Link>
+          <Navbar.Link isActive={isMyListingsSelected} onClick={()=>{ 
+            setIsMyListingsSelected(true)
+            setIsExploreSelected(false)
+            setIsMyBookingsSelected(false)
+            }} href="#">My Listings</Navbar.Link>
+        </Navbar.Content>
+        <Navbar.Content
+          css={{
+            "@xs": {
+              w: "12%",
+              jc: "flex-end",
+            },
+          }}
+        >
+          <Dropdown placement="bottom-right">
+            <Navbar.Item>
+              <Dropdown.Trigger>
+                <Avatar
+                  bordered
+                  as="button"
+                  color="secondary"
+                  size="md"
+                  src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                />
+              </Dropdown.Trigger>
+            </Navbar.Item>
+            <Dropdown.Menu
+              aria-label="User menu actions"
+              color="secondary"
+              onAction={(actionKey) => console.log({ actionKey })}
+            >
+              <Dropdown.Item key="profile" css={{ height: "$18" }}>
+                <Text b color="inherit" css={{ d: "flex" }}>
+                  Signed in as
+                </Text>
+                <Text b color="inherit" css={{ d: "flex" }}>
+                  zoey@example.com
+                </Text>
+              </Dropdown.Item>
+              <Dropdown.Item key="settings" withDivider>
+                My Settings
+              </Dropdown.Item>
+              <Dropdown.Item key="team_settings">Team Settings</Dropdown.Item>
+              <Dropdown.Item key="analytics" withDivider>
+                Analytics
+              </Dropdown.Item>
+              <Dropdown.Item key="system">System</Dropdown.Item>
+              <Dropdown.Item key="configurations">Configurations</Dropdown.Item>
+              <Dropdown.Item key="help_and_feedback" withDivider>
+                Help & Feedback
+              </Dropdown.Item>
+              <Dropdown.Item key="logout" withDivider color="error">
+                Log Out
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Navbar.Content>
+        <Navbar.Collapse>
+          {collapseItems.map((item, index) => (
+            <Navbar.CollapseItem
+              key={item}
+              activeColor="secondary"
+              css={{
+                color: index === collapseItems.length - 1 ? "$error" : "",
+              }}
+              isActive={index === 2}
+            >
+              <Link
+                color="inherit"
+                css={{
+                  minWidth: "100%",
+                }}
+                href="#"
+              >
+                {item}
+              </Link>
+            </Navbar.CollapseItem>
+          ))}
+        </Navbar.Collapse>
+      </Navbar>
+    </Layout>
+  );
+}
